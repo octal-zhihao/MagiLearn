6c07eef65588e5d1a57b4b0c4ea8a6fdec55a4cb
llz <2460752764@qq.com>
2024-12-15 03:36:12 +0800

    docx: add models

--- magilearn/models/__init__.py
+++ magilearn/models/__init__.py
@@ -1,7 +1,7 @@
 from .logistic_regression import LogisticRegression
 from .linear_regression import LinearRegression
 from .k_means import KMeans
-from .logistic_regression import LogisticRegression
 from .decision_tree_classifier import DecisionTreeClassifier
+from .random_forest import RandomForestClassifier
 
-__all__ = ['LogisticRegression', 'LinearRegression', 'KMeans', 'LogisticRegression', 'DecisionTreeClassifier']
+__all__ = ['LogisticRegression', 'LinearRegression', 'KMeans', 'LogisticRegression', 'DecisionTreeClassifier', 'RandomForestClassifier']
