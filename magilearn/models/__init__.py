d5dde07f09d11d70fb46b4fc015462bd92e5ec70
llz <2460752764@qq.com>
2024-12-16 01:47:52 +0800

    feat: add gradient_boosting

--- magilearn/models/__init__.py
+++ magilearn/models/__init__.py
@@ -3,5 +3,6 @@ from .linear_regression import LinearRegression
 from .k_means import KMeans
 from .decision_tree import DecisionTreeClassifier, DecisionTreeRegressor
 from .random_forest import RandomForestClassifier
+from .gradient_boosting import GradientBoostingClassifier
 
-__all__ = ['LogisticRegression', 'LinearRegression', 'KMeans', 'LogisticRegression', 'DecisionTreeClassifier', 'DecisionTreeRegressor', 'RandomForestClassifier']
+__all__ = ['LogisticRegression', 'LinearRegression', 'KMeans', 'LogisticRegression', 'DecisionTreeClassifier', 'DecisionTreeRegressor', 'RandomForestClassifier', 'GradientBoostingClassifier']
