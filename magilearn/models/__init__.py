fb54597aef8c09b7d51744dc1692e5cb496476ac
llz <2460752764@qq.com>
2024-12-16 01:21:23 +0800

    feat: add decision_tree_regressor

--- magilearn/models/__init__.py
+++ magilearn/models/__init__.py
@@ -1,7 +1,7 @@
 from .logistic_regression import LogisticRegression
 from .linear_regression import LinearRegression
 from .k_means import KMeans
-from .decision_tree_classifier import DecisionTreeClassifier
+from .decision_tree import DecisionTreeClassifier, DecisionTreeRegressor
 from .random_forest import RandomForestClassifier
 
-__all__ = ['LogisticRegression', 'LinearRegression', 'KMeans', 'LogisticRegression', 'DecisionTreeClassifier', 'RandomForestClassifier']
+__all__ = ['LogisticRegression', 'LinearRegression', 'KMeans', 'LogisticRegression', 'DecisionTreeClassifier', 'DecisionTreeRegressor', 'RandomForestClassifier']
